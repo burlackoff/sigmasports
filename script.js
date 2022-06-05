c1e9919a2f3bce107e59fe0a5fa315e314197ea7
Stepan Burlakov <burlackoff@gmail.com>
2022-06-05 18:55:25 +0300

    style: add style form

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ buttonSlide.addEventListener('click', () => {
   buttonSlide.classList.toggle('switch__slider_active');
   buttonSlide.classList.toggle('switch__slider_active::before');
 })
+
+const form = document.querySelector('.form');
+const formInput = document.querySelector('.form__input');
+const buttonForm = document.querySelector('.form__button');
+
+
+formInput.onfocus = () => {
+  buttonForm.classList.add('form__button_active');
+}
+
+formInput.onblur = () => {
+  buttonForm.classList.remove('form__button_active');
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+  formInput.value = 'Круто!';
+  formInput.setAttribute("disabled", "disabled");
+  formInput.style.cursor = "auto";
+  buttonForm.classList.remove('form__button_active');  
+}
+
+buttonForm.onmousedown = (event) => {
+  if (document.activeElement === formInput) {
+    event.preventDefault();
+  }
+};
