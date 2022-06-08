2536cf960c7568b2a737503d0948ecd7e2c7b69a
Stepan Burlakov <burlackoff@gmail.com>
2022-06-08 21:58:06 +0300

    feat: add slider icon

--- script.js
+++ script.js
@@ -44,16 +44,23 @@ const slides = document.querySelectorAll('.gradient-road__image');
 const counterSlide = slides.length;
 const btnNext = document.querySelector('.gradient-road__button_right');
 const btnPrev = document.querySelector('.gradient-road__button_left');
+const sliderIcon = document.querySelector('.gradient-road__icons');
+const slidesIcon = document.querySelectorAll('.gradient-road__icon');
+const iconCounter = slidesIcon.length;
+let activeIconIndex = 0;
 
+function init() {
+  let counter = 0;
+  slides.forEach(item => {
+    item.style.left = counter * 690 + 'px';
+    counter++;
+  })
+  slidesIcon[activeIconIndex].style.left = 100 + 'px';
+}
 
-let counter = 0;
-slides.forEach(item => {
-  item.style.left = counter * 690 + 'px';
-  counter++;
-})
+init()
 
 btnNext.addEventListener('click', nextSlide);
-
 btnPrev.addEventListener('click', nextSlide);
 
 
@@ -70,17 +77,22 @@ function nextSlide(){
 function next() {
   btnNext.removeEventListener('click', nextSlide);
   btnPrev.removeEventListener('click', nextSlide);
+  slideIcon('next')
+
   let slides2 = document.querySelectorAll('.gradient-road__image');
   let slider = document.querySelector('.gradient-road__images');
+
   slides2.forEach(item => {
     item.style.left = item.offsetLeft - 690 + 'px';
   })
   
   setTimeout(() => {
     let slide = slides2[0].cloneNode(true);
+
     slide.style.left = (slides2.length - 1) *690 + 'px';
     slider.append(slide);
     slides2[0].remove();
+
     btnNext.addEventListener('click', nextSlide);
     btnPrev.addEventListener('click', nextSlide);
   }, 1000); 
@@ -89,14 +101,17 @@ function next() {
 function prev() {
   btnNext.removeEventListener('click', nextSlide);
   btnPrev.removeEventListener('click', nextSlide);
+  slideIcon('prev')
+  
   let slides3 = document.querySelectorAll('.gradient-road__image');
   let slider1 = document.querySelector('.gradient-road__images');
-
   let slide = slides3[slides3.length - 1].cloneNode(true);
+
   slide.style.left = -690 + 'px';
   slider1.insertBefore(slide, slides3[0]);
   slides3[slides3.length - 1].remove();
   slides3 = document.querySelectorAll('.gradient-road__image');
+
   slides3.forEach(item => {
     item.style.left = item.offsetLeft + 690 + 'px';
   })
@@ -107,4 +122,22 @@ function prev() {
   }, 1000);
 }
 
+
+function slideIcon(direction) {
+  slidesIcon[activeIconIndex].style.left = -100 + 'px';
+  if (direction === 'next') {
+    activeIconIndex++
+    if (activeIconIndex === iconCounter) {
+      activeIconIndex = 0;
+    }
+  } else if (direction === 'prev') {
+    activeIconIndex--
+    if (activeIconIndex < 0) {
+      activeIconIndex = iconCounter - 1;
+    }
+  }
+  setTimeout(() => {
+    slidesIcon[activeIconIndex].style.left = 100 + 'px'
+  }, 500)
+}
 //finish
